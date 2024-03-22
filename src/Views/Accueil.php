c6165626c4eabbdee3e54f4deb87954255504fec
Joana <joana.rodrigues.simplon@gmail.com>
2024-03-22 17:01:46 +0100

    IMG add

--- src/Views/Accueil.php
+++ src/Views/Accueil.php
@@ -9,8 +9,8 @@ require "include/navbar.php";
     <h2>Liste de professeurs</h2>
 
     <div class="professeur flex">
-        <img src="" alt="">
-        <div class="infoCard">
+        <img src="public/img/prof.jpg" alt="Photo de profil de professeur">
+        <div class="infoCard flex">
             <h3>Nom - Prénom</h3>
             <p>Some quick example text to build on the card title and make up the bulk of the card's content.<br>
             30€/h</p>
